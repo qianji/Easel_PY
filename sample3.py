71ad688d2001567f7f5cea9f2801720d0a31c482
qianji zheng <qianji.zheng@gmail.com>
2015-04-10 16:37:02 -0500

    re-design the code for ball dropping

--- sample3.py
+++ sample3.py
@@ -1,6 +1,7 @@
 """-------------------------------------------------------------------
 
-Program Pingpong Bouncing
+Program: Ball droping, bouncing and rolling
+This is a sample program illustrating how to load a image from a file and a sound from a file and play the sound
 by Qianji Zheng
 Texas Tech Dept. of Computer Science
 
@@ -8,48 +9,128 @@ Texas Tech Dept. of Computer Science
 
 
 from EaselLib import *
+######################################################################
+# windowDimensions: >> tuple
+#
+# set the window of game 800 pixel wide and 600 pixel high
 
+def windowDimensions():
+    return (800,600)
+
+######################################################################
 def init():
-    global Ball_Position, Ball_Img,Ball,V_Y,V_X,Hit_Sound,Boundary
-    # "ball.png" must be in the folder called media
-    Boundary = (-350,-250)
+    global Ball_Location, Ball_Img,Velocity_Y,Velocity_X,Hit_Sound,Ground,Button_Img, Button_Location
+    # load the ball image and button image from "pingpong.png" and "start_button.png" respectively
+    # The image files must be in the folder called "media" in the game engine, where this program is in
+    # You need to remember the size of your image file.
+
+    # the size of the pingpong image is 50 x 50
     Ball_Img = loadImageFile("pingpong.png")
-    # set the initial position of the ball
-    Ball_Position = (-350,-250)
-    Ball = fileImg(Ball_Img,Ball_Position)
+    # the size of the start button is 64 x 64
+    Button_Img = loadImageFile("start_button.png")
+
+    # set the initial location of the ball. The ball image's left-top point is (-350,100)
+    Ball_Location = (-350,100)
+    # set the location of the restart button. The button image's left-top point is (150,150)
+    Button_Location = (150,150)
+    # load the sound from a file. The file pingpong.wav must be put in the folder called media, where this program is in
     Hit_Sound = loadSoundFile("pingpong.wav")
-    V_Y = 15
-    V_X = 5
+    # set the initial velocity of the ball
+    Velocity_Y = -15
+    Velocity_X = 5
+    # make the ground
+    Ground = (-350,-250)
 
+######################################################################
 def update():
-    global Ball_Position,Ball
-    Ball_Position = next_ball_position(Ball_Position)
-    #Ball_Position = (Ball_Position[0] +10,Ball_Position[1])
-    Ball = fileImg(Ball_Img,Ball_Position)
+    # ball movement
+    if ball_is_in_the_air():
+        drop_ball()
+    if ball_hits_ground():
+        bounce_ball()
+    if ball_is_on_ground():
+        roll_ball()
+    # clicking the button
+    if start_button_clicked():
+        init()
+######################################################################
+# when the ball is droppring, assume that it has acceleration of gravity
+# and no air resistance
+#
+def drop_ball():
+    global Velocity_Y,Velocity_X, Ball_Location
+    x,y = Ball_Location
+    # assume that there is no air resistance 
+    next_x = x + Velocity_X
+    # mimic acceleration of gravity
+    acceleration = -0.98
+    Velocity_Y = Velocity_Y+acceleration
+    next_y = y+(Velocity_Y + 0.5*acceleration)
+    if next_y<Ground[1]:
+        next_y=Ground[1]
+    Ball_Location = (next_x,next_y)
+######################################################################
+def bounce_ball():
+    global Velocity_Y, Ball_Location
+    x,y = Ball_Location
+    # update ball location
+    # reverse the dicrection of the velocity of y and slow it down to bounce to air
+    Velocity_Y = -Velocity_Y -10
+    if Velocity_Y <0:
+        Velocity_Y = 0
+    acceleration = -0.98
+    next_y = y+(Velocity_Y + 0.5*acceleration)
+    next_x = x + Velocity_X
+    if next_y<Ground[1]:
+        next_y=Ground[1]
+    Ball_Location = (next_x,next_y)
+    # play the sound
+    playSound(Hit_Sound)
+######################################################################
+def roll_ball():
+    global Velocity_X
+    # slow down the ball
+    Velocity_X = Velocity_X -0.5
+    if Velocity_X <0:
+        Velocity_X = 0
 
-def next_ball_position(pos):
-    global V_Y,V_X
-    x,y = pos
-    a = -0.98
-    ax = -0.2
-    next_x = x + V_X
-    V_Y = V_Y+a
-    V_X = V_X
-    next_y = y+(V_Y + 0.5*a)
-    if next_y < Boundary[1]:
-        next_y = Boundary[1]
-        V_Y = -V_Y
+######################################################################
+# ball_hits_ground: tuple -> Bool
+# the ball hits the ground if its y-axis is less than or equal to the y-axis
+# of the ground and the velocity of y-axis of the ball is not equal to 0
 
-    if next_y==Boundary[1]:
-        V_X =V_X-0.5
-        if V_X<0:
-            V_X=0
-            V_Y=0
-    # play the sound when the ball hits the ground
-    if next_y<=Boundary[1] and (V_Y>0 or V_X>0):
-        playSound(Hit_Sound)
-    return (next_x,next_y)
+def ball_hits_ground():
+    return Ball_Location[1]<=Ground[1] and Velocity_Y !=0
 
+######################################################################
+def ball_is_on_ground():
+    return Ball_Location[1]==Ground[1]
+######################################################################
+def ball_is_in_the_air():
+    return not ball_is_on_ground()
+
+######################################################################
+# ball_in_game: >> Sprite
+#
+def ball_in_game():
+    return [fileImg(Ball_Img,Ball_Location)]
+######################################################################
+# click: >> Bool
+#
+# click() means the left mouse button has gone from up to down.
+
+def click(): 
+    return mouseDown and not oldMouseDown
+
+######################################################################
+def start_button_clicked():
+    x,y = Button_Location
+    return click() and x<mouseX<x+64 and y-64<mouseY<y
+######################################################################
+
+def start_button():
+    return [fileImg(Button_Img,Button_Location)]
+######################################################################
 def display():
-    return [Ball]
+    return ball_in_game()+start_button()
 
